d3b0f6a5a3801c02271b703b2cec600bf167b440
kimmhyeon <kimmhyeon@gmail.com>
2021-08-04 17:16:01 +0900

    210804 마우스 드래그기능 코드 변경

---
+++ Assets/H&G/9Epi/DragObject.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Test");
+    }
+
+    private void OnMouseDrag()
+    {
+        Debug.Log("작동");
+    }
+
+    private void OnDrag()
+    {
+        Debug.Log("작동");
+    }
+}
