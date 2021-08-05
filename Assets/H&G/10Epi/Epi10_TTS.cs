03ac0e6e517fe20bf4f848afa09d561cf25a0045
ooo2o5 <88296556+ooo2o5@users.noreply.github.com>
2021-08-05 15:04:45 +0900

    Epi10_TTS_Reload

---
+++ Assets/H&G/10Epi/Epi10_TTS.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Epi10_TTS : MonoBehaviour
+{
+private bool mb_CheckOncePlaying = true;
+    private VoiceManager mvm_PlayVoiceManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mvm_PlayVoiceManager = GameObject.Find("VoiceManager").GetComponent<VoiceManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mvm_PlayVoiceManager.mb_checkSceneReady && mb_CheckOncePlaying) {
+            mvm_PlayVoiceManager.playVoice(0);
+            mb_CheckOncePlaying = false;
+        }
+        if (!mvm_PlayVoiceManager.isPlaying() && mvm_PlayVoiceManager.mb_checkSceneReady) {
+            Invoke("nextSceneLoad", 2f);
+        }
+    }
+    void nextSceneLoad() {
+        SceneManager.LoadScene("1_11H&G");
+    }
+}
