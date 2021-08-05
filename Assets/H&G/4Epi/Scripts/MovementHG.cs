f0ec058dc8fb0d668284246676ef6c463123d429
devYoong <73592778+lykee2000@users.noreply.github.com>
2021-08-05 13:24:50 +0900

    210805 modify Epi4 Script

--- Assets/H&G/4Epi/Scripts/MovementHG.cs
+++ Assets/H&G/4Epi/Scripts/MovementHG.cs
@@ -13,10 +13,13 @@ public class MovementHG : MonoBehaviour
     float gratelX = 0.0f;
     float HanselX = 0.0f;
 
-    private GameObject rock1;
-    private GameObject rock2;
-    private GameObject rock3;
+    // private GameObject rock1;
+    // private GameObject rock2;
+    // private GameObject rock3;
 
+    // private Image RockLeft;
+    // private Image RockMiddle;
+    // private Image RockRight;
     private Color RockLeft;
     private Color RockMiddle;
     private Color RockRight;
@@ -31,13 +34,15 @@ public class MovementHG : MonoBehaviour
         gratelX = GratelPos.anchoredPosition.x;
         HanselX = HanselPos.anchoredPosition.x;
 
-        rock1 = GameObject.Find("rock1");
-        rock2 = GameObject.Find("rock2");
-        rock3 = GameObject.Find("rock3");
+        // rock1 = GameObject.Find("rock1");
+        // rock2 = GameObject.Find("rock2");
+        // rock3 = GameObject.Find("rock3");
 
-        RockLeft = rock1.GetComponent<Image>().color;
-        RockMiddle = rock2.GetComponent<Image>().color;
-        RockRight = rock3.GetComponent<Image>().color;
+        // RockLeft = rock1.GetComponent<Image>();
+        // RockMiddle = rock2.GetComponent<Image>();
+        // RockRight = rock3.GetComponent<Image>();
+
+        
         
         // Gratel.GetComponent<RectTransform>().anchoredPosition = new Vector2(110,-290);
 
@@ -47,14 +52,31 @@ public class MovementHG : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       if(GratelPos.anchoredPosition.x > 110.0f){
-           gratelX -= 4.0f;
+       if(GratelPos.anchoredPosition.x > -115.0f){
+           gratelX -= 13.0f;
            GratelPos.anchoredPosition = new Vector3(gratelX,-309.0f);
        }
-        if(HanselPos.anchoredPosition.x > 490.0f){
-           HanselX -= 4.0f;
+        if(HanselPos.anchoredPosition.x > 220.0f){
+            if(HanselPos.anchoredPosition.x <= 682.0f){
+            //    RockRight.color = new Color(RockRight.color.r,RockRight.color.g,RockRight.color.b,1.0f);
+                GameObject.Find("rock3").GetComponent<Image>().color = new Color(1.0f,1.0f,1.0f,1.0f);   
+                if(HanselPos.anchoredPosition.x <= 442.0f){
+                    GameObject.Find("rock2").GetComponent<Image>().color = new Color(1.0f,1.0f,1.0f,1.0f);   
+                    if(HanselPos.anchoredPosition.x <= 260.0f){
+                        GameObject.Find("rock1").GetComponent<Image>().color = new Color(1.0f,1.0f,1.0f,1.0f);   
+                    }                
+                }
+
+            }
+            // else if(HanselPos.anchoredPosition.x <= 442.0f){
+            //     GameObject.Find("rock2").GetComponent<Image>().color = new Color(1.0f,1.0f,1.0f,1.0f);   
+            // }
+            // else if(HanselPos.anchoredPosition.x <= 260.0f){
+            //     GameObject.Find("rock1").GetComponent<Image>().color = new Color(1.0f,1.0f,1.0f,1.0f);   
+            // }
+           HanselX -= 13.0f;
            HanselPos.anchoredPosition = new Vector3(HanselX,-245.0f);
-        //    if 
+
        }
     }
 
