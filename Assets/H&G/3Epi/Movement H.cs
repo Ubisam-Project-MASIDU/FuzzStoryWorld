bd934727e7c984001a44bb58622e734ebc93b011
Ryuhyis <76957700+Ryuhyis@users.noreply.github.com>
2021-08-05 17:05:08 +0900

    epi3

---
+++ Assets/H&G/3Epi/Movement H.cs
@@ -0,0 +1,27 @@
+/*
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MovementH : MonoBehaviour
+{
+    public int Speed;
+    private GameObject Hansel;
+
+    // Start is called before the first frame update
+ 
+    void Start()
+    {
+        Hansel = GameObject.Find("Hansel");
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.G)
+    
+    }
+}
+*/
