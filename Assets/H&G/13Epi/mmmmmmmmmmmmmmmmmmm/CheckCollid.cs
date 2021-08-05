b718ee2521f4c77cf3fd31bea1a0c74d51054355
kimmhyeon <kimmhyeon@gmail.com>
2021-08-05 14:52:22 +0900

    210805 시나리오13 - 아이템생성되게 설정

---
+++ Assets/H&G/13Epi/mmmmmmmmmmmmmmmmmmm/CheckCollid.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckCollid : MonoBehaviour
+{
+    bool mb_IsCollision = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D cCollidObj)
+    {
+        Debug.Log("충돌 감지");
+        mb_IsCollision = true;
+    }
+
+    void OnTriggerExit2D(Collider2D cCollidObj)
+    {
+        Debug.Log("충돌 끝");
+        mb_IsCollision = false;
+    }
+
+    public bool b_IsCollision()
+    {
+        return mb_IsCollision;
+    }
+}
