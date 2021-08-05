ebd17c25eb3d1b178acf76d525e1a3019e41de5c
devYoong <73592778+lykee2000@users.noreply.github.com>
2021-08-05 15:53:17 +0900

    210805 modify Epi4 movements

--- Assets/H&G/4Epi/Scripts/MovementHG.cs
+++ Assets/H&G/4Epi/Scripts/MovementHG.cs
@@ -7,81 +7,21 @@ public class MovementHG : MonoBehaviour
 {
     private GameObject Gratel;
     private GameObject Hansel;
-    private RectTransform GratelPos;
-    private RectTransform HanselPos;
-
-    float gratelX = 0.0f;
-    float HanselX = 0.0f;
-
-    // private GameObject rock1;
-    // private GameObject rock2;
-    // private GameObject rock3;
-
-    // private Image RockLeft;
-    // private Image RockMiddle;
-    // private Image RockRight;
-    private Color RockLeft;
-    private Color RockMiddle;
-    private Color RockRight;
-    // Start is called before the first frame update
+    private GameObject targetPosition;
+    private Vector3 GratelPos;
+    private Vector3 HanselPos;
+    IEnumerator GratelMove;
+    IEnumerator HanselMove;
     void Start(){
         Gratel = GameObject.Find("Gratel");
         Hansel = GameObject.Find("Hansel");
-
-        GratelPos = Gratel.GetComponent<RectTransform>();
-        HanselPos = Hansel.GetComponent<RectTransform>();
-
-        gratelX = GratelPos.anchoredPosition.x;
-        HanselX = HanselPos.anchoredPosition.x;
-
-        // rock1 = GameObject.Find("rock1");
-        // rock2 = GameObject.Find("rock2");
-        // rock3 = GameObject.Find("rock3");
-
-        // RockLeft = rock1.GetComponent<Image>();
-        // RockMiddle = rock2.GetComponent<Image>();
-        // RockRight = rock3.GetComponent<Image>();
-
         
-        
-        // Gratel.GetComponent<RectTransform>().anchoredPosition = new Vector2(110,-290);
-
-        // Hansel.GetComponent<RectTransform>().anchoredPosition = new Vector2(110,-290);
+        GratelPos = new Vector3(1.0f,-6.1f,20.0f);
+        HanselPos = new Vector3(2.0f,-5.9f,20.0f);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-       if(GratelPos.anchoredPosition.x > -115.0f){
-           gratelX -= 13.0f;
-           GratelPos.anchoredPosition = new Vector3(gratelX,-309.0f);
-       }
-        if(HanselPos.anchoredPosition.x > 220.0f){
-            if(HanselPos.anchoredPosition.x <= 682.0f){
-            //    RockRight.color = new Color(RockRight.color.r,RockRight.color.g,RockRight.color.b,1.0f);
-                GameObject.Find("rock3").GetComponent<Image>().color = new Color(1.0f,1.0f,1.0f,1.0f);   
-                if(HanselPos.anchoredPosition.x <= 442.0f){
-                    GameObject.Find("rock2").GetComponent<Image>().color = new Color(1.0f,1.0f,1.0f,1.0f);   
-                    if(HanselPos.anchoredPosition.x <= 260.0f){
-                        GameObject.Find("rock1").GetComponent<Image>().color = new Color(1.0f,1.0f,1.0f,1.0f);   
-                    }                
-                }
-
-            }
-            // else if(HanselPos.anchoredPosition.x <= 442.0f){
-            //     GameObject.Find("rock2").GetComponent<Image>().color = new Color(1.0f,1.0f,1.0f,1.0f);   
-            // }
-            // else if(HanselPos.anchoredPosition.x <= 260.0f){
-            //     GameObject.Find("rock1").GetComponent<Image>().color = new Color(1.0f,1.0f,1.0f,1.0f);   
-            // }
-           HanselX -= 13.0f;
-           HanselPos.anchoredPosition = new Vector3(HanselX,-245.0f);
-
-       }
+    void Update(){
+        Gratel.transform.position = Vector3.MoveTowards(Gratel.transform.position, GratelPos, 0.05f);
+        Hansel.transform.position = Vector3.MoveTowards(Hansel.transform.position, HanselPos, 0.05f);
     }
-
-    // public void ChangeHGRectTransform(){
-    //     GratelPos.anchoredPosition = new Vector3(110,-245,0);
-    //     HanselPos.anchoredPosition = new Vector3(110,-245,0);
-    // }
 }
