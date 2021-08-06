d78d4bb9944c66c4513f7d65676de7b90d474314
Ryuhyis <76957700+Ryuhyis@users.noreply.github.com>
2021-08-06 11:43:47 +0900

    Scene3 미니게임 완료
    추가 수정필요

--- Assets/H&G/3Epi/Collision.cs
+++ Assets/H&G/3Epi/Collision.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 public class Collision : MonoBehaviour
 {
     Vector3 mv3_initPos;
-    public bool locked = false;
-    public Sprite sNextSprite;
+   
     // Start is called before the first frame update
     void Start()
     {
@@ -16,14 +15,14 @@ public class Collision : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        this.transform.position = Vector3.MoveTowards(this.transform.position, mv3_initPos, 10f * Time.deltaTime);
+        this.transform.position = Vector3.MoveTowards(this.transform.position, mv3_initPos, 11f * Time.deltaTime);
     }
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (Input.GetMouseButtonUp(0))
         {
             Debug.Log("1");
-            if (collision.name[collision.name.Length - 1] == this.name[this.name.Length - 1])
+            if (collision.tag == this.tag)
             {
                 Debug.Log("2");
                 Destroy(this.gameObject);
