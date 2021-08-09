8ddc5aa72032a80bb1065972c97f23fb3c2d572e
devYoong <73592778+lykee2000@users.noreply.github.com>
2021-08-09 14:22:25 +0900

    210809 폴더정리

--- Assets/H&G/08Epi/Scripts/ClickCandy.cs
+++ Assets/H&G/08Epi/Scripts/ClickCandy.cs
@@ -1,10 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.EventSystems;
 public class ClickCandy : MonoBehaviour
 {
-    private int mn_LeftVirus = 10; 
+    private int mn_Click = 5; 
     // Start is called before the first frame update
     void Start()
     {
@@ -15,5 +15,15 @@ public class ClickCandy : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("클릭");
+        this.mn_Click-= 1;
+        if(this.mn_Click == 0){
+            Debug.Log("제거");
+            Destroy(this.gameObject);
+        }
     }
 }
