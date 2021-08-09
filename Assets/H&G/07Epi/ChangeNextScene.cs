5a76ca87fb5bb06ec49e121b381a75cd8c3cfc1b
choidj <69896751+choidj@users.noreply.github.com>
2021-08-09 17:57:28 +0900

    add annotations...

--- Assets/H&G/07Epi/ChangeNextScene.cs
+++ Assets/H&G/07Epi/ChangeNextScene.cs
@@ -1,11 +1,39 @@
+/*
+ * - Name : ChangeNextScene.cs
+ * - Writer : 최대준
+ * 
+ * - Content :
+ * 헨젤과 그레텔 게임 오브젝트가 해당 스크립트 클래스 컴포넌트가 들어가 있는 오브젝트에 부딪히면, 1초 후에 changeNextScene함수를 호출하여 씬을 다음 씬으로 옮기도록 하였다.
+ * 
+ * - History
+ * 1) 2021-08-05 : 코드 구현. 
+ * 2) 2021-08-06 : 주석 작성.
+ *  
+ * - Variable 
+ * null
+ * 
+ * - Function
+* OnTriggerEnter(Collider cCollider)                      해당 오브젝트의 컴포넌트인 콜라이더 박스에 다른 콜라이더 박스가 닿았을 때, 호출되는 함수이다. 여기서는 헨젤과 그레텔 오브젝트와 닿았을 때 v_changeNextScene 1초후에 호출하도록 하여 다음 씬으로 넘어가도록 하였다.
+ */
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+// 다음 씬으로 넘어가는 동작을 정의한 스크립트 클래스이다.
 public class ChangeNextScene : MonoBehaviour {
-    void OnTriggerEnter(Collider cCollider) {
-        if (cCollider.tag == "HAG")
-            Invoke("changeNextScene", 1f);
+    
+    // 해당 오브젝트의 컴포넌트에서 콜라이더 박스에 무언가 닿았다면 호출된다.
+    private void OnTriggerEnter(Collider cCollider) {
+        if (cCollider.tag == "HAG") {
+            Invoke("v_changeNextScene", 1f);
+        }
     }
+
+    // 다음 씬을 호출하는 함수이다.
+    private void v_changeNextScene() {
+        SceneManager.LoadScene("1_08H&G");
+    }
+    
 }
--- Assets/H&G/07Epi/ClickAnim.cs
+++ Assets/H&G/07Epi/ClickAnim.cs
@@ -1,21 +1,42 @@
+/*
+ * - Name : ClickAnim.cs
+ * - Writer : 최대준
+ * 
+ * - Content :
+ * 깜빡이는 동작을 정의한 스크립트 클래스이다.
+ * 
+ * - History
+ * 1) 2021-08-05 : 코드 구현. 
+ * 2) 2021-08-06 : 주석 작성.
+ *  
+ * - Variable 
+ * mf_CountTime                                     시간을 재기 위한 변수이다.
+ * mn_CountSecond                                   시간을 재기 위한 변수이다. 1 2 3 4 5 ... 의 초단위 시간이 지난 것을 표현한다.
+ *
+ * - Function
+ * Update()                                         1초 마다 오브젝트의 그림을 보였다가 안 보여 줬다가 해서, 깜빡이는 효과를 표현하였다.
+ */
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ClickAnim : MonoBehaviour
-{
-    private float mf_CountTime = 0f;
-    private int mn_CountSecond = 1;
-    void Update()
-    {
-
+// 깜빡이는 효과를 표현하는 스크립트 클래스이다.
+public class ClickAnim : MonoBehaviour {
+    private float mf_CountTime = 0f;        // 소수점 단위의 시간을 표현한다.
+    private int mn_CountSecond = 1;         // 정수 단위의 시간을 표현한다.
+    
+    // 업데이트 함수에서는 1초 마다 그림을 껏다 켰다 하는 동작을 표현한다.
+    void Update() {
         mf_CountTime += Time.deltaTime;
+        
         if (mn_CountSecond < mf_CountTime) {
             mn_CountSecond++;
-            if (this.GetComponent<SpriteRenderer>().enabled)
+            if (this.GetComponent<SpriteRenderer>().enabled) {
                 this.GetComponent<SpriteRenderer>().enabled = false;
-            else
+            } else {
                 this.GetComponent<SpriteRenderer>().enabled = true;
+            }
         }
     }
 }
