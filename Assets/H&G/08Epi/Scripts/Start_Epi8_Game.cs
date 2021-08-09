f061f20d7d846a469a6aa3762daf1e0aa8475b66
devYoong <73592778+lykee2000@users.noreply.github.com>
2021-08-09 13:50:53 +0900

    210809 08Epi 스크립트

--- Assets/H&G/08Epi/Scripts/Start_Epi8_Game.cs
+++ Assets/H&G/08Epi/Scripts/Start_Epi8_Game.cs
@@ -1,37 +1,51 @@
+/*
+ * - Name : Start_Epi8_Game.class
+ * - Writer : 이윤교
+ * - Content : 헨젤과그레텔 Epi8 _ Epi8 미니게임 시작 스크립트
+ * 
+ * - HISTORY
+ * 2021-08-09 : 초기 개발 및 코드 획일화, 주석처리
+ * 2021-08-06 : 코드 획일화 및 주석처리
+ *
+ * <Variable>
+ * mgo_Gratel : 그레텔 게임오브젝트
+ * mgo_Hansel : 헨젤 게임오브젝트
+ * mgo_Target : 문 위치 게임오브젝트
+ * mv3_Position : 문 위치 벡터화
+ * <Function>
+ * ChangePosition(GameObject go_Object,Vector3 v3_Pos,float f_Velocity) : go_Object가  v3_Pos위치로 f_Velocity 속력으로 이동하는 함수 
+ */
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Start_Epi8_Game : MonoBehaviour
 {
-    private GameObject Gratel;
-    private GameObject Hansel;
-    private GameObject Target;
-    private Vector3 Position;
+    //헨젤과 그레텔 이동에 필요한 변수들
+    private GameObject mgo_Gratel;
+    private GameObject mgo_Hansel;
+    private GameObject mgo_Target;
+    private Vector3 mv3_Position;
 
     VoiceManager vm;
 
     // Start is called before the first frame update
     void Start()
     {
-        Gratel = GameObject.Find("Gratel");
-        Hansel = GameObject.Find("Hansel");
-        Target = GameObject.Find("Target");
-        Position = Target.transform.position;
-        // Position = new Vector3(6.4f,5.2f,-14.6f);
+        mgo_Gratel = GameObject.Find("Gratel");
+        mgo_Hansel = GameObject.Find("Hansel");
+        mgo_Target = GameObject.Find("Target");
+        mv3_Position = mgo_Target.transform.position;                               //문 위치 벡터화
 
         this.vm = GameObject.Find("VoiceManager").GetComponent<VoiceManager>();
     }
 
     // Update is called once per frame
     void Update()
-    {
-        // ChangePosition(Gratel,Position,0.05f);
-        // ChangePosition(Hansel,Position,0.05f);
-        
+    {   
         if (vm.mb_checkSceneReady){                                                 //tts 사용전 작업이 다 준비되면
-            ChangePosition(Gratel,Position,0.05f);
-            ChangePosition(Hansel,Position,0.05f);
+            ChangePosition(mgo_Gratel,mv3_Position,0.05f);                                  //그레텔 이동
+            ChangePosition(mgo_Hansel,mv3_Position,0.05f);                                  //헨젤 이동
         
         }
     }
