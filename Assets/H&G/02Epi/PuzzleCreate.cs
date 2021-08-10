8b062ee95206ed40c3695be6dd033bdd541f5484
Ryuhyis <76957700+Ryuhyis@users.noreply.github.com>
2021-08-10 17:17:59 +0900

    epi2 minigame -ing

---
+++ Assets/H&G/02Epi/PuzzleCreate.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PuzzleCreate : MonoBehaviour
+{
+
+    SpriteSlice Sprite [2,3] sc ;
+    public Texture2D mgo_PuzzleImage;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        mgo_PuzzleImage = GameObject.Find("PuzzleImage").GetComponent<Texture2D>();
+        sc.sliceSprite(2,3, mgo_PuzzleImage);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
