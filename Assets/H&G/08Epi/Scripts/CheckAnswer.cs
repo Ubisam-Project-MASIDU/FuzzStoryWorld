abd87ba19558ec025ce1e8f725e550a7d2e3981c
devYoong <73592778+lykee2000@users.noreply.github.com>
2021-08-11 11:49:28 +0900

    210811 Epi8 mini game 코드 획일화 및 주석처리

--- Assets/H&G/08Epi/Scripts/CheckAnswer.cs
+++ Assets/H&G/08Epi/Scripts/CheckAnswer.cs
@@ -1,56 +1,69 @@
+/*
+ * - Name : CheckAnswer.class
+ * - Writer : 이윤교
+ * - Content : 헨젤과 그레텔 Epi8 미니 게임 - 카트 오브젝트에 관한 스크립트
+ *                                     - 충돌 시 정답 확인 및 정답일 경우 오브젝트 삭제
+ * - HISTORY
+ * 2021-08-10 : 초기 개발
+ * 2021-08-11 : 코드 획일화 및 주석처리
+ *
+ * <Variable>
+ * mg_GameDirector : GameDirector오브젝트 연결을 위한 변수
+ * mg_RandomItem : RandomItem 오브젝트 연결을 위한 변수
+ * mn_answer : 정답을 저장해두는 변수
+ * mn_LeftTime : 남은 횟수를 저장해두는 변수
+ *
+ * <Function>
+ * OnTriggerEnter2D(Collider2D cCollidObj) : 오브젝트가 충돌됬을때 작동되는 함수
+ * 
+ */
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CheckAnswer : MonoBehaviour
-{
+public class CheckAnswer : MonoBehaviour{
     GameObject mg_GameDirector;
     GameObject mg_RandomItem;
     int mn_answer;
     int mn_LeftTime;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    void Start(){
         this.mg_GameDirector = GameObject.Find("GameDirector");                                         // 게임오브젝트들 연결
         this.mg_RandomItem = GameObject.Find("RandomImage");
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        mn_answer = mg_RandomItem.GetComponent<RandomCandy>().n_ReturnAnswer();                       // 실시간으로 정답을 전달받는다
+    void Update(){
+        mn_answer = mg_RandomItem.GetComponent<RandomCandy>().n_ReturnAnswer();                         // 실시간으로 정답을 전달받는다
     }
-
     void OnTriggerEnter2D(Collider2D cCollidObj){
         if(cCollidObj.tag == "Pink"){
-            if (mn_answer == 0){
+            if (mn_answer == 0){                                                                        // 정답인 경우
                 Debug.Log("Pink Candy 먹기 성공");
-                mg_GameDirector.GetComponent<CandyControl>().v_CheckRandomItemArr(0);                 // 정답배열에 해당 아이템이 정답처리가 되었다고 표시
-                mn_LeftTime = mg_GameDirector.GetComponent<CandyControl>().n_HowManyleftArr();            // 남은 횟수 확인 및 값 저장
+                mg_GameDirector.GetComponent<CandyControl>().v_CheckRandomItemArr(0);                   // 정답배열에 해당 아이템이 정답처리가 되었다고 표시
+                mn_LeftTime = mg_GameDirector.GetComponent<CandyControl>().n_HowManyleftArr();          // 남은 횟수 확인 및 값 저장
                 Destroy(cCollidObj.gameObject);
-                mg_GameDirector.GetComponent<CandyControl>().v_ChangeFlagTrue();                          // 정답이 되었다고 flag값 변경  
-            }else{
+                mg_GameDirector.GetComponent<CandyControl>().v_ChangeFlagTrue();                        // 정답이 되었다고 flag값 변경  
+            }else{                                                                                      // 오답인 경우
                 Debug.Log("Pink Candy 먹기 실패");
             }
         }else if(cCollidObj.tag == "Yellow"){
-            if (mn_answer == 1){
+            if (mn_answer == 1){                                                                        // 정답인 경우
                 Debug.Log("Yellow Candy 먹기 성공");
-                mg_GameDirector.GetComponent<CandyControl>().v_CheckRandomItemArr(1);                 // 정답배열에 해당 아이템이 정답처리가 되었다고 표시
-                mn_LeftTime = mg_GameDirector.GetComponent<CandyControl>().n_HowManyleftArr();            // 남은 횟수 확인 및 값 저장
+                mg_GameDirector.GetComponent<CandyControl>().v_CheckRandomItemArr(1);                   // 정답배열에 해당 아이템이 정답처리가 되었다고 표시
+                mn_LeftTime = mg_GameDirector.GetComponent<CandyControl>().n_HowManyleftArr();          // 남은 횟수 확인 및 값 저장
                 Destroy(cCollidObj.gameObject);
-                mg_GameDirector.GetComponent<CandyControl>().v_ChangeFlagTrue();                          // 정답이 되었다고 flag값 변경  
-            }else{
-                Debug.Log("Yellow Candy 먹기 실패");
+                mg_GameDirector.GetComponent<CandyControl>().v_ChangeFlagTrue();                        // 정답이 되었다고 flag값 변경  
+            }else{                                                                                      // 오답인 경우
+                Debug.Log("Yellow Candy 먹기 실패");    
             }
         }else if(cCollidObj.tag == "Blue"){
-            if (mn_answer == 2){
+            if (mn_answer == 2){                                                                        // 정답인 경우
                 Debug.Log("Blue Candy 먹기 성공");
-                mg_GameDirector.GetComponent<CandyControl>().v_CheckRandomItemArr(2);                 // 정답배열에 해당 아이템이 정답처리가 되었다고 표시
-                mn_LeftTime = mg_GameDirector.GetComponent<CandyControl>().n_HowManyleftArr();            // 남은 횟수 확인 및 값 저장
+                mg_GameDirector.GetComponent<CandyControl>().v_CheckRandomItemArr(2);                   // 정답배열에 해당 아이템이 정답처리가 되었다고 표시
+                mn_LeftTime = mg_GameDirector.GetComponent<CandyControl>().n_HowManyleftArr();          // 남은 횟수 확인 및 값 저장
                 Destroy(cCollidObj.gameObject);
-                mg_GameDirector.GetComponent<CandyControl>().v_ChangeFlagTrue();                          // 정답이 되었다고 flag값 변경  
-            }else{
+                mg_GameDirector.GetComponent<CandyControl>().v_ChangeFlagTrue();                        // 정답이 되었다고 flag값 변경  
+            }else{                                                                                      // 오답인 경우
                 Debug.Log("Blue Candy 먹기 실패");
             }
         }
--- Assets/H&G/08Epi/Scripts/DragCandy.cs
+++ Assets/H&G/08Epi/Scripts/DragCandy.cs
@@ -1,39 +1,43 @@
+/*
+ * - Name : DragCandy.class
+ * - Writer : 이윤교
+ * - Content : 헨젤과 그레텔 Epi8 미니게임 - 마우스 이벤트 스크립트
+ *                                   - 마우스 드래그시 오브젝트가 따라 움직이게 수정
+ *                                   - 마우스에서 손을 뗄 경우 오브젝트 원래위치로 이동
+ * - HISTORY
+ * 2021-08-10 : 초기 개발
+ * 2021-08-11 : 코드 획일화 및 주석처리
+ *
+ * <Variable>
+ * mv2_mouseDragPosition        마우스 위치를 저장하는 벡터
+ * mv2_worldObjectPosition      카메라의 월드좌표로 변환을 위한 벡터
+ *
+ * <Function>
+ * OnMouseDown()                오브젝트를 클릭한 경우
+ * OnMouseDrag()                오브젝트를 드래그한 경우
+ * OnMouseUp()                  오브젝트에서 손을 떼는 경우
+ * 
+ */
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DragCandy : MonoBehaviour
-{
+public class DragCandy : MonoBehaviour{
     private Vector3 screenSpace;
     private Vector3 offset;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     
     // 오브젝트를 클릭한 경우
     private void OnMouseDown(){
-        //translate the cubes position from the world to Screen Point
-        screenSpace = Camera.main.WorldToScreenPoint(transform.position);
-        //calculate any difference between the cubes world position and the mouses Screen position converted to a world point 
-        offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenSpace.z));
+        screenSpace = Camera.main.WorldToScreenPoint(transform.position);                                                                           // 오브젝트의 월드 좌표를 스크린 좌표로 변환
+        offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenSpace.z));     // 오브젝트 월드벡터 - 마우스 월드벡터 (벡터끼리의 차는 서로의 거리와 방향을 뜻함. 여기서는 마우스에서 오브젝트까지의 벡터)
     }
 
     // 오브젝트를 드래그한 경우
     private void OnMouseDrag(){
-        //keep track of the mouse position
-        var curScreenSpace = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenSpace.z);
-        //convert the screen mouse position to world point and adjust with offset
-        var curPosition = Camera.main.ScreenToWorldPoint(curScreenSpace) + offset;
-        //update the position of the object in the world
-        transform.position = curPosition;
+        var curScreenSpace = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenSpace.z);                                              // 현재 마우스의 스크린좌표
+        var curPosition = Camera.main.ScreenToWorldPoint(curScreenSpace) + offset;                                                                  // 마우스 벡터 + 마우스->오브젝트 벡터 = 마우스 월드 좌표
+        transform.position = curPosition;                                                                                                           // 오브젝트 위치 바꾸기
         Debug.Log("드래그앤 드롭 작동중");
 
     }
@@ -41,8 +45,8 @@ public class DragCandy : MonoBehaviour
     // 오브젝트에서 손을 떼는 경우
     private void OnMouseUp(){
         Debug.Log("오브젝트에서 손 뗌");
-        if(this.tag == "Pink"){                                                       // 오브젝트에 따라
-            this.transform.position = new Vector3(1.986f, 1.86f, -7.54f);                          // 원래 위치로 이동
+        if(this.tag == "Pink"){                                                                                                                     // 오브젝트에 따라
+            this.transform.position = new Vector3(1.986f, 1.86f, -7.54f);                                                                           // 원래 위치로 이동
         }
         if (this.tag == "Yellow"){
             this.transform.position = new Vector3(2.178f, 0.955f, -7.54f);
