0c40c429ece351573992461d14160cb1352e9494
ooo2o5 <88296556+ooo2o5@users.noreply.github.com>
2021-08-11 16:54:07 +0900

    Epi_10_MonsterDestory&GoToNextEpi

---
+++ Assets/H&G/10Epi/ControlMonster.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ControlMonster : MonoBehaviour
+{
+    int num = 6;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(num<=0){
+            SceneManager.LoadScene("1_11H&G");
+        }
+    }
+
+    public void Delete(){
+        num--;
+    }
+}
---
+++ Assets/H&G/10Epi/KilltheMonster.cs
@@ -0,0 +1,48 @@
+/*
+ * - Name : KilltheMonster.cs
+ * - Writer : 이병권
+ * 
+ * - Content : 
+ * 쓰레기 몬스터 설정 스크립트
+ * 몇번을 터치하여 죽게할 것인지 설정
+ *            
+ *            
+ * -수정 기록-
+ * 2021-08-10 : 제작 완료
+ * 2021-08-11 : 주석 작성
+ * 
+ * - Variable 
+ * T_Monster_HP : 쓰레기 몬스터 HP 설정 변수
+*
+ * -Function()
+ * OnMouseDown() : 바이러스 클릭시 작동되는 함수
+ * 
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KilltheMonster : MonoBehaviour
+{
+    private int T_Monster_HP = 2;               // 세균의 채력 범위 설정
+    int n_num = 6;
+    GameObject ControlMonster;
+    
+    void Start(){
+        ControlMonster = GameObject.Find("ControlMonster");
+    }
+
+    private void OnMouseDown()
+    {   
+        if (T_Monster_HP <= 0){                  // 세균의 HP가 0보다 작으면 죽는경우 설정
+            ControlMonster.GetComponent<ControlMonster>().Delete();
+            Destroy(gameObject);
+        }
+        else
+        {
+            T_Monster_HP -= 1;                   // 세균을 터치하여 HP감소
+            Debug.Log("클릭");
+        }
+    }
+}
