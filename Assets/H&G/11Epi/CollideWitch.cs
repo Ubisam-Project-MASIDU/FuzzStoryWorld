c147638dad05b81afa3156309729c016f586512c
ooo2o5 <88296556+ooo2o5@users.noreply.github.com>
2021-08-11 14:33:05 +0900

    Epi10, 11 - Take a Name tag on Scripts

--- Assets/H&G/11Epi/CollideWitch.cs
+++ Assets/H&G/11Epi/CollideWitch.cs
@@ -1,18 +1,33 @@
+/*
+ * - Name : CollideWitch.cs
+ * - Writer : 이병권
+ * - Content : 헨젤과그레텔Epi11 - 헨젤과그레텔이 가지고 있는 뼈 막대기와 마녀가 만났을때 사라지게 하는 내용
+ 
+ * - HISTORY (수정기록)
+ * 2021-08-5 : 초기 개발
+ * 2021-08-9 : 파일 인코딩 수정
+ * 2021-08-11 : 주석 처리 수정
+ 
+ * <Function> 쓴것들에 대하여 설명
+ * OnTriggerEnter : 해당 트리거 콜라이더와 트리거와 접촉한 강체(or 강체가 없다면 콜라이더)로 보내집니다 (충돌)
+ * cCollider.tag : 충돌해야 하는 물체를 이름을 설정해서 지정해주는 것
+ * Destory : 충돌하였을 때 사라지게 하는 것
+ */
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CollideWitch : MonoBehaviour
+public class CollideWitch : MonoBehaviour 
 {
 
     void OnTriggerEnter(Collider cCollider) {
 
-        if (cCollider.tag == "bb1" ){
-            Destroy(cCollider.gameObject);          
+        if (cCollider.tag == "bb1" ){                 // bb1(그레텔이 가지고 있는 뼈다기 이름을 backbone인데 그것을 줄여서 bb로 지정하고 사라지게 하기 위해 이름을 붙여줌) 
+            Destroy(cCollider.gameObject);            // 만약 위에 bb1일 Witch(마녀)에 3D에 다았을 때 사라지게 함
         }    
         
-        else if (cCollider.tag == "bb2" ){
-            Destroy(cCollider.gameObject);          
+        else if (cCollider.tag == "bb2" ){           // bb2(헨젤이 가지고 있는 뼈다기 이름을 backbone인데 그것을 줄여서 bb로 지정하고 사라지게 하기 위해 이름을 붙여줌) 
+            Destroy(cCollider.gameObject);           // 만약 위에 bb1일 Witch(마녀)에 3D에 다았을 때 사라지게 함  
         }  
     }
 }
--- Assets/H&G/11Epi/DragBoneStick.cs
+++ Assets/H&G/11Epi/DragBoneStick.cs
@@ -1,3 +1,31 @@
+/*
+ * - Name : DragBoneStick.cs
+ * - Writer : 이병권
+ * - Content : 헨젤과그레텔Epi11 - 마우스로 뼈다귀를 이용 할 수 있는 스크랩
+                                   (핵심 내용)
+ *                                 -1) 마우스로 오브젝트를 움직이게 한다
+ *                                 -2) 마우스에서 손을 떼었을 때 오브젝트가 원래위치로 이동
+ 
+ * - HISTORY (수정기록)
+ * 2021-08-5 : 초기 개발
+ * 2021-08-9 : 파일 인코딩 수정
+ * 2021-08-11 : 주석 처리 수정
+ 
+ * <Variable> 쓴것들에 대하여 설명
+ * mv2_mouseDragPosition        마우스 위치를 저장하는 벡터
+ * mv2_worldObjectPosition      카메라의 월드좌표로 변환을 위한 벡터
+ *
+ * <Function>
+ * OnMouseDown()                오브젝트를 클릭한 경우
+ * OnMouseDrag()                오브젝트를 드래그한 경우
+ * OnMouseUp()                  오브젝트에서 손을 떼는 경우
+
+ * OnTriggerEnter : 해당 트리거 콜라이더와 트리거와 접촉한 강체(or 강체가 없다면 콜라이더)로 보내집니다 (충돌)
+ * cCollider.tag : 충돌해야 하는 물체를 이름을 설정해서 지정해주는 것
+ * Destory : 충돌하였을 때 사라지게 하는 것
+ */
+
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,43 +34,28 @@ public class DragBoneStick : MonoBehaviour
 {
     private Vector3 screenSpace;
     private Vector3 offset;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    
     // 오브젝트를 클릭한 경우
     private void OnMouseDown(){
-        //translate the cubes position from the world to Screen Point
         screenSpace = Camera.main.WorldToScreenPoint(transform.position);
-        //calculate any difference between the cubes world position and the mouses Screen position converted to a world point 
         offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenSpace.z));
     }
 
     // 오브젝트를 드래그한 경우
     private void OnMouseDrag(){
-        //keep track of the mouse position
         var curScreenSpace = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenSpace.z);
-        //convert the screen mouse position to world point and adjust with offset
         var curPosition = Camera.main.ScreenToWorldPoint(curScreenSpace) + offset;
-        //update the position of the object in the world
         transform.position = curPosition;
         Debug.Log("드래그앤 드롭 작동중");
 
     }
 
     // 오브젝트에서 손을 떼는 경우
+    // 오브젝트에 따라 원래 위치로 이동
     private void OnMouseUp(){
         Debug.Log("오브젝트에서 손 뗌");
-        if(this.tag == "bb1"){                                                       // 오브젝트에 따라
-            this.transform.position = new Vector3(-1.32f, 1.02f, -7.5f);                          // 원래 위치로 이동
+        if(this.tag == "bb1"){                                                    
+            this.transform.position = new Vector3(-1.32f, 1.02f, -7.5f);             
         }
         if (this.tag == "bb2"){
             this.transform.position = new Vector3(1.0f, 1.02f, -7.5f);
