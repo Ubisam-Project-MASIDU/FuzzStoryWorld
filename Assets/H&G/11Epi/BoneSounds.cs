efbda82ca1abf50b5faa5da4b00a7ba9ccb04906
ooo2o5 <88296556+ooo2o5@users.noreply.github.com>
2021-08-12 15:27:01 +0900

    Epi11

---
+++ Assets/H&G/11Epi/BoneSounds.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoneSounds : MonoBehaviour
+{
+   public AudioClip audioAttack;
+
+   public AudioClip audioDie;
+   AudioSource audioSource;
+
+   void Awake(){
+
+       this.audioSource = GetComponent<AudioSource>();
+   }
+ 
+   public void PlaySound(string action) {
+       switch(action) {
+           case "ATTACK":
+                audioSource.clip = audioAttack;
+                break;
+            case "DIE" :
+                audioSource.clip =  audioDie;
+                break;
+
+        }
+        audioSource.Play();
+   }
+   
+
+}
