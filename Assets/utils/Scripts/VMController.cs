24a65b2ae55decd9fdcd4a55dcc010439970dd53
choidj <69896751+choidj@users.noreply.github.com>
2021-08-12 16:43:23 +0900

    Update VMController.cs

--- Assets/utils/Scripts/VMController.cs
+++ Assets/utils/Scripts/VMController.cs
@@ -1,3 +1,37 @@
+/*
+ * - Name : VMController.cs
+ * - Writer : 최대준
+ * 
+ * - Content :
+ * 다양한 언어를 로드하고, 언어에 따라서 다른 음성을 출력하도록 구현된 스크립트 코드이다.
+ * 
+ * - History
+ * 1) 2021-08-05 : 코드 구현. 
+ * 2) 2021-08-09 : 주석 작성.
+ *  
+ * - Variable 
+ * public enum Country {
+     KR,
+     EN,
+     JP,
+     CN
+ } 현재 선택된 언어를 표현하기 위한 enum 변수이다.
+ * mn_LanguageLength        현재 지원되는 언어의 숫자이다.
+ * mn_CurrentVMIdx          현재 로딩중인 언어의 인덱스 숫자를 표현한다.
+ * mb_CheckSceneReady       현재 언어들의 로딩이 다 끝났는지를 표현해주는 변수이다.
+ * mb_CheckLanguageReady    현재 언어들의 로딩이 다 끝났는지를 표현해주는 변수이다.
+ * mgo_loadingScene         원래는 VoiceManager 클래스에 있던 로딩 화면 오브젝트인데, 다양한 언어를 지원하기 위해 VMController 클래스에서 담당하도록 하였다.
+ * mc_loadingScene          원래는 VoiceManager 클래스에 있던 로딩 화면 프리팹인데, 다양한 언어를 지원하기 위해 VMController 클래스에서 담당하도록 하였다.
+ * mct_CheckCountry         현재 선택된 언어를 뜻한다.
+ *
+ * - Function
+ * Update()                     클릭 이벤트가 발생되면, 레이져를 쏜 후에 그곳에 오브젝트가 있다면, 어떤 오브젝트인지를 switch문으로 판별하여 그에 따른 언어를 로드하게 되어 있다.
+ * Awake()
+ * 
+ * 
+ * 
+ */
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,7 +47,7 @@ public class VMController : MonoBehaviour {
 
     public int mn_LanguageLength = 4;
     private int mn_CurrentVMIdx = 0;
-    public bool mb_checkSceneReady = false;
+    public bool mb_CheckSceneReady = false;
     public bool mb_CheckLanguageReady = false;
     private GameObject mgo_loadingScene;
     public GameObject mc_loadingScene;
@@ -26,8 +60,8 @@ public class VMController : MonoBehaviour {
     }
     
     void Update() {
-        if (mn_CurrentVMIdx >= mn_LanguageLength && !mb_checkSceneReady) {
-            mb_checkSceneReady = true;
+        if (mn_CurrentVMIdx >= mn_LanguageLength && !mb_CheckSceneReady) {
+            mb_CheckSceneReady = true;
             mgo_loadingScene.SetActive(false);
             return;
         }
