4ab4c2bff721410af8a52f6285af1bcdd5e52a7c
leeyeeun <thsudkcla7@naver.com>
2021-08-13 15:55:57 +0900

    Epi12 마녀 움직임 수정

--- Assets/H&G/12Epi/MoveWitch.cs
+++ Assets/H&G/12Epi/MoveWitch.cs
@@ -1,1 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+public class MoveWitch : MonoBehaviour
+{
+    public GameObject mgo_HanselGretel;
+    SpriteRenderer spriteRenderer;
+    Animator anim;
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per fram
+    void Update()
+    {     
+        if(Mathf.Abs(mgo_HanselGretel.transform.position.x - transform.position.x) < 1.0) {
+            anim.SetBool("Walking", false);
+
+        } else {
+            anim.SetBool("Walking", true);
+            transform.position = Vector3.MoveTowards(transform.position, mgo_HanselGretel.transform.position, 1.2f * Time.deltaTime);
+        }
+        if(mgo_HanselGretel.transform.position.x - transform.position.x < 0) {
+            spriteRenderer.flipX = true;
+        }
+    }
+    void OnTriggerEnter(Collider collision)
+    {
+        this.transform.GetChild(0).gameObject.SetActive(true);
+    }
+}
+    
