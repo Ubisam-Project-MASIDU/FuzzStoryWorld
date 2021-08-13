d6ff7c7d6840cf774c7916f262813d5edf3b18e2
devYoong <73592778+lykee2000@users.noreply.github.com>
2021-08-13 16:37:44 +0900

    210813 Epi5 RunGame

--- Assets/H&G/05Epi/runner/PlayerController.cs
+++ Assets/H&G/05Epi/runner/PlayerController.cs
@@ -2,22 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     bool isJump = false;
     bool isTop = false;
     public float jumpHeight = 0;
     public float jumpSpeed = 0; 
     Vector2 startPosition;
+    Animator animator;
     // Start is called before the first frame update
     void Start()
     {
         startPosition = transform.position;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        animator.SetBool("Run",true);
         if(Input.GetMouseButtonDown(0)){
             isJump = true;
         }else if(transform.position.y <= startPosition.y){
