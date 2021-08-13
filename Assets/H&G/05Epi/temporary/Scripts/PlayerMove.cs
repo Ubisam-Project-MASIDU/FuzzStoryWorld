0e22bfe48b6bbbd3559ab1de1900bec3dbf4a525
devYoong <73592778+lykee2000@users.noreply.github.com>
2021-08-13 14:23:18 +0900

    runnergame

--- Assets/H&G/05Epi/temporary/Scripts/PlayerMove.cs
+++ Assets/H&G/05Epi/temporary/Scripts/PlayerMove.cs
@@ -6,15 +6,23 @@ public class PlayerMove : MonoBehaviour
 {
     Rigidbody2D rigid;
     public float maxSpeed;
+    public float jumpPower;
     SpriteRenderer spriteRenderer;
+    Animator animator;
     // Start is called before the first frame update
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
 
     private void Update(){
+        //Jump
+        if(Input.GetButtonDown("Jump") && !animator.GetBool("isJumping")){
+            rigid.AddForce(Vector2.up*jumpPower , ForceMode2D.Impulse);
+            animator.SetBool("isJumping",true);        
+        }
         //Stop Speed
         if(Input.GetButtonUp("Horizontal")){
             //벡터 크기를 1로 만든 상태(단위벡터)
@@ -25,6 +33,15 @@ public class PlayerMove : MonoBehaviour
         if(Input.GetButtonDown("Horizontal")){
             spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
         }
+
+        //Animation
+        if(Mathf.Abs(rigid.velocity.x) < 0.3){
+            animator.SetBool("isWalking", false);
+        }
+        else{
+            animator.SetBool("isWalking", true);
+        }
+
     }
     // Update is called once per frame
     void FixedUpdate()
@@ -38,5 +55,16 @@ public class PlayerMove : MonoBehaviour
      }else if(rigid.velocity.x < maxSpeed*(-1)){ //Left Max Speed, 방향이 음수이므로 x-1
         rigid.velocity = new Vector2(maxSpeed*(-1),rigid.velocity.y);
      }
+
+        //Landing Platform
+        if(rigid.velocity.y < 0){
+            Debug.DrawRay(rigid.position, Vector3.down, new Color(0,1,0)); //에디터 상에서만 Ray를 그려주는 함수
+            RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
+            if(rayHit.collider != null){ //RaycastHit변수의 콜라이더로 검색 확인 기능
+                if(rayHit.distance < 0.5f){
+                    animator.SetBool("isJumping",false);
+                }
+            }
+        }
     }
 }
