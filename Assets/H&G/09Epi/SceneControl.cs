98196ff0d2d488c5141a7bd31ac093f327151376
Ryuhyis <76957700+Ryuhyis@users.noreply.github.com>
2021-08-18 16:46:16 +0900

    dd

--- Assets/H&G/09Epi/SceneControl.cs
+++ Assets/H&G/09Epi/SceneControl.cs
@@ -8,15 +8,21 @@ public class SceneControl : MonoBehaviour
 
     public VoiceManager mvm_VoiceManager;
     private bool mb_PlayFirstVoice = false;                                                                                         // 첫번째 나레이션의 실행 유무를 위한 flag
-
+    private Animator fearAnimation;
     public GameObject mg_WitchText;
     public GameObject vibrate;
     public Text mt_Text;
+    public bool hidestartflag = false;
+    private SpriteRenderer render;
+
     // Start is called before the first frame update
     void Start()
     {
         mvm_VoiceManager = GameObject.Find("VoiceManager").GetComponent<VoiceManager>();
+        fearAnimation = GetComponent<Animator>();
         mg_WitchText.SetActive(false);
+        render = GameObject.Find("Background").GetComponent<SpriteRenderer>();
+
     }
 
     // Update is called once per frame
@@ -24,10 +30,13 @@ public class SceneControl : MonoBehaviour
     {
         if (!mb_PlayFirstVoice)
         {                                                             // 나레이션1 실행조건 검사
-            mvm_VoiceManager.playVoice(0);                                                                                          // 나레이션1과 playVoice(0) 연결됨
+            mvm_VoiceManager.playVoice(0);
+            Debug.Log("1");
             mb_PlayFirstVoice = true;
             Invoke("SecondEvent", 4f);
             Invoke("ThirdEvent", 8f);
+            Invoke("FourthEvent", 12f);
+            Invoke("ColorChange", 15f);
             // 나레이션1 출력 완료 
         }
     }
@@ -39,12 +48,26 @@ public class SceneControl : MonoBehaviour
     void SecondEvent()
     {
         mg_WitchText.SetActive(true);
+        Debug.Log("2");
     }
 
     void ThirdEvent()
     {
         mg_WitchText.SetActive(false);
         mt_Text.text = "\n       사실 그 할머니는 아이들을 잡아먹는 무서운 마녀였어요.        \n";
-        mvm_VoiceManager.playVoice(1);                                                                                          // 나레이션1과 playVoice(0) 연결됨
+        mvm_VoiceManager.playVoice(1);
+        Debug.Log("3");// 나레이션1과 playVoice(0) 연결됨
     }
+    
+    void FourthEvent()
+    {
+        mt_Text.text = "\n       물체를 클릭해서 마녀에게로부터 헨젤과 그레텔을 숨겨주세요.        \n";
+        hidestartflag = true;
+    }
+
+    void ColorChange()
+    {
+        render.color = new Color(85 / 255f, 85 / 255f, 85 / 255f, 255 / 255f);
+    }
+
 }
