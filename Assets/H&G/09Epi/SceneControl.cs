271461ba2aa786ae6854cef3e1dd01d3f6696ca7
Ryuhyis <76957700+Ryuhyis@users.noreply.github.com>
2021-08-18 15:33:59 +0900

    ddd

--- Assets/H&G/09Epi/SceneControl.cs
+++ Assets/H&G/09Epi/SceneControl.cs
@@ -1,16 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SceneControl : MonoBehaviour
 {
 
     public VoiceManager mvm_VoiceManager;
     private bool mb_PlayFirstVoice = false;                                                                                         // 첫번째 나레이션의 실행 유무를 위한 flag
-    private bool mb_PlaySecondVoice = false;
-    private bool mb_WitchTextFlag = false;
+
     public GameObject mg_WitchText;
     public GameObject vibrate;
+    public Text mt_Text;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,32 +22,29 @@ public class SceneControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (mvm_VoiceManager.mb_checkSceneReady && !mb_PlayFirstVoice)
+        if (!mb_PlayFirstVoice)
         {                                                             // 나레이션1 실행조건 검사
             mvm_VoiceManager.playVoice(0);                                                                                          // 나레이션1과 playVoice(0) 연결됨
-            mb_PlayFirstVoice = true;                                                                                               // 나레이션1 출력 완료 
+            mb_PlayFirstVoice = true;
+            Invoke("SecondEvent", 4f);
+            Invoke("ThirdEvent", 8f);
+            // 나레이션1 출력 완료 
         }
-
-        if (mvm_VoiceManager.isPlaying() == false && mb_PlayFirstVoice)
-        {
-            mg_WitchText.SetActive(true);
-            mb_WitchTextFlag = true;
-            Invoke("mg_PopUp", 2f);
-        }
-
-        if (mb_WitchTextFlag && !mb_PlaySecondVoice)
-        {
-           
-        }
-
-        //if (mvm_VoiceManager.isPlaying() == false && mvm_VoiceManager.mb_checkSceneReady && mb_PlaySecondVoice)
-        //{                    // 나레이션2까지 출력 끝나면 다음씬으로 이동
-          //  mg_Popup.SetActive(true);                                                                                               // 다음씬으로 이동하기 위한 함수
-        //}
     }
 
     void StopVibrate()
     {
         vibrate.SetActive(false);
+    }
+    void SecondEvent()
+    {
+        mg_WitchText.SetActive(true);
+    }
+
+    void ThirdEvent()
+    {
+        mg_WitchText.SetActive(false);
+        mt_Text.text = "\n       사실 그 할머니는 아이들을 잡아먹는 무서운 마녀였어요.        \n";
+        mvm_VoiceManager.playVoice(1);                                                                                          // 나레이션1과 playVoice(0) 연결됨
     }
 }
