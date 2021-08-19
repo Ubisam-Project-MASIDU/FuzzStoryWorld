b82c0541169805e05f66d00b04744e2de6b7a40a
choidj <69896751+choidj@users.noreply.github.com>
2021-08-19 10:43:20 +0900

    update Status scripts and scene 12

---
+++ Assets/H&G/12Epi/NotifyPlayerDanger.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotifyPlayerDanger : MonoBehaviour {
+    private ParticleSystem mps_WaitEffect;
+    private GameObject mgo_NotifyPlayerBox;
+
+    void Start() {
+        mps_WaitEffect = GetComponent<ParticleSystem>();
+        mps_WaitEffect.Stop(true);
+        mgo_NotifyPlayerBox = transform.GetChild(0).gameObject;
+        Invoke("StartAttack", 2f);
+    }
+
+    void StartAttack() {
+        mgo_NotifyPlayerBox.GetComponent<MeshRenderer>().enabled = false;
+        mps_WaitEffect.Play(true);
+
+        Invoke("StopAttack", 1f);
+    }
+
+    void StopAttack() {
+        Destroy(this);
+    }
+}
---
+++ Assets/H&G/12Epi/Status.cs
@@ -0,0 +1,70 @@
+public class Status {
+    public enum Character {
+        HAG,
+        Witch,
+        Length      //enum의 종류의 길이를 뜻한다. 캐릭터를 추가하려면, 꼭 이 위에 해주길 바란다.
+    }
+    private Character mch_Character = Character.HAG;
+    private float mf_HP = 100f;
+    #region define mf_HP setter and getter...
+    public float HP {
+        get {
+            return mf_HP;
+        }
+        set {
+            mf_HP = value;
+        }
+    }
+    #endregion
+    private float mf_AttackDamage = 2f;
+    #region define mf_AttackDamage setter and getter...
+    public float AttackDamage {
+        get {
+            return mf_AttackDamage;
+        }
+        set {
+            mf_AttackDamage = value;
+        }
+    }
+    #endregion
+    private float mf_WalkSpeed = 1f;
+    #region define mf_WalkSpeed setter and getter...
+    public float WalkSpeed {
+        get {
+            return mf_WalkSpeed;
+        }
+        set {
+            mf_WalkSpeed = value;
+        }
+    }
+    #endregion
+    
+    // this is constructor..
+    #region 
+    public Status() {}
+    public Status(Character chCharacter) {
+        mch_Character = chCharacter;
+    }
+
+    public Status(float fHP) {
+        mf_HP = fHP;
+    }
+    public Status(float fHP, float fAttackDamage) {
+        mf_HP = fHP;
+        mf_AttackDamage = fAttackDamage;
+    }
+
+    public Status(float fHP, float fAttackDamage, float fWalkSpeed) {
+        mf_HP = fHP;
+        mf_AttackDamage = fAttackDamage;
+        mf_WalkSpeed = fWalkSpeed;
+    }
+
+    public Status(float fHP, float fAttackDamage, float fWalkSpeed, Character chCharacter) {
+        mf_HP = fHP;
+        mf_AttackDamage = fAttackDamage;
+        mf_WalkSpeed = fWalkSpeed;
+        mch_Character = chCharacter;
+    }
+    #endregion
+}
