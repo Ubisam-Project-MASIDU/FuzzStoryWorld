3562c74ca96dface09da0e0184cba6405671a844
devYoong <73592778+lykee2000@users.noreply.github.com>
2021-08-25 13:32:50 +0900

    클릭클릭

--- Assets/Intro/SelectManager.cs
+++ Assets/Intro/SelectManager.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class SelectManager : MonoBehaviour{
+    //private bool mb_checkClick = false;
     void Update(){
-        if (Input.GetMouseButtonDown(0)){
-            Debug.Log("Click");
-            SceneManager.LoadScene("0_01LanguageSelect");
-        }
+        // if (Input.GetMouseButtonDown(0)){
+        //     //mb_checkClick = true;
+        //     Debug.Log("Click");
+            
+        // }
     }
-
+    void OnMouseDown(){
+        Debug.Log("Click");
+    }
+    // void OnTriggerEnter2D(Collider2D cCollidObj){
+    //     if(cCollidObj.tag == "헨젤과그레텔"){
+    //         SceneManager.LoadScene("0_01LanguageSelect");
+    //     }
+    // }
     // // Start is called before the first frame update
     // void Start()
     // {
