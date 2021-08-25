bbbcfdd419bd7cff8ce3230e612e19348fe96968
kimmhyeon <kimmhyeon@gmail.com>
2021-08-25 16:35:48 +0900

    210825 StartingScene - 씬 앞으로가기 뒤로가기 구현

---
+++ Assets/utils/Scripts/MoveSceneMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MoveSceneMenu : MonoBehaviour
+{
+    public string ms_NextSceneName;
+    public string ms_PreviousSceneName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void MoveNextScene()
+    {
+        Debug.Log("씬 이동버튼 클릭");
+        SceneManager.LoadScene(ms_NextSceneName);
+    }
+
+    public void MovePreviousScene()
+    {
+        Debug.Log("씬 이동버튼 클릭");
+        SceneManager.LoadScene(ms_PreviousSceneName);
+    }
+}
