9e861b21b9b55af1833ba39f024d935d3d87db72
Ryuhyis <76957700+Ryuhyis@users.noreply.github.com>
2021-08-25 16:20:55 +0900

    언어재선택->자막도 같이 변경

--- Assets/utils/Scripts/ChangeCountry.cs
+++ Assets/utils/Scripts/ChangeCountry.cs
@@ -7,10 +7,13 @@ using UnityEngine.UI;
 public class ChangeCountry : MonoBehaviour
 {
     private VoiceManager mvm_VoiceManager;
+    private CaptionControl cc;
+    private string temp;
     public int mn_PlayVoiceIndex;
 
     void Start() {
         mvm_VoiceManager = FindObjectOfType<VoiceManager>();
+        cc = GameObject.Find("CaptionPanel").GetComponent<CaptionControl>();
     }
     
     public void OnclickButton() {
@@ -20,24 +23,36 @@ public class ChangeCountry : MonoBehaviour
                 mvm_VoiceManager.stopVoice();
                 mvm_VoiceManager.country = VoiceManager.Country.KR;
                 mvm_VoiceManager.playVoice(mn_PlayVoiceIndex);
+                temp = "";
+                cc.mt_CaptionText.GetComponent<Text>().text = temp;
+                //cc.gameObject.SetActive(true);
                 break;
             case "usa": 
                 Debug.Log("미국");
                 mvm_VoiceManager.stopVoice();
                 mvm_VoiceManager.country = VoiceManager.Country.EN;
-                mvm_VoiceManager.playVoice(mn_PlayVoiceIndex);            
+                mvm_VoiceManager.playVoice(mn_PlayVoiceIndex);
+                temp = mvm_VoiceManager.mlva_LanguageVoices[1].mvifl_setVoiceInfoList[cc.mn_VoicIndex].words;
+                cc.mt_CaptionText.GetComponent<Text>().text = temp;
+                cc.gameObject.SetActive(true);
                 break;
             case "japan": 
                 Debug.Log("일본");
                 mvm_VoiceManager.stopVoice();
                 mvm_VoiceManager.country = VoiceManager.Country.JP;
-                mvm_VoiceManager.playVoice(mn_PlayVoiceIndex);             
+                mvm_VoiceManager.playVoice(mn_PlayVoiceIndex);
+                temp = mvm_VoiceManager.mlva_LanguageVoices[2].mvifl_setVoiceInfoList[cc.mn_VoicIndex].words;
+                cc.mt_CaptionText.GetComponent<Text>().text = temp;
+                cc.gameObject.SetActive(true);
                 break;
             case "china": 
                 Debug.Log("중국");
                 mvm_VoiceManager.stopVoice();
                 mvm_VoiceManager.country = VoiceManager.Country.CN;
-                mvm_VoiceManager.playVoice(mn_PlayVoiceIndex);       
+                mvm_VoiceManager.playVoice(mn_PlayVoiceIndex);
+                temp = mvm_VoiceManager.mlva_LanguageVoices[3].mvifl_setVoiceInfoList[cc.mn_VoicIndex].words;
+                cc.mt_CaptionText.GetComponent<Text>().text = temp;
+                cc.gameObject.SetActive(true);
                 break;
         }       
     }
