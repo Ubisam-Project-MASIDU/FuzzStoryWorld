3db83fec805b81b9d8250f38f28f1d6fea8a75af
devYoong <73592778+lykee2000@users.noreply.github.com>
2021-08-25 16:25:08 +0900

    Select씬 구현중

--- Assets/Intro/SelectManager.cs
+++ Assets/Intro/SelectManager.cs
@@ -3,36 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class SelectManager : MonoBehaviour{
-    //private bool mb_checkClick = false;
     void Update(){
-        // if (Input.GetMouseButtonDown(0)){
-        //     //mb_checkClick = true;
-        //     Debug.Log("Click");
-            
-        // }
-    }
-    void OnMouseDown(){
-        Debug.Log("Click");
-    }
-    // void OnTriggerEnter2D(Collider2D cCollidObj){
-    //     if(cCollidObj.tag == "헨젤과그레텔"){
-    //         SceneManager.LoadScene("0_01LanguageSelect");
-    //     }
-    // }
-    // // Start is called before the first frame update
-    // void Start()
-    // {
-        
-    // }
 
-    // // Update is called once per frame
-    // void Update()
-    // {
-        
-    // }
+    }
 
-    // private void OnMouseDown(){
-    //     Debug.Log("Click");
-    //     SceneManager.LoadScene("0_01LanguageSelect");
-    // }
+    public void First(){
+        SceneManager.LoadScene("0_01LanguageSelect");
+    }
+    public void Second(){
+        //Support.SetActive(true);
+    }
+    public void Third(){
+        //SceneManager.LoadScene("New Scene");
+    }
+    public void Fourth(){
+        //SceneManager.LoadScene("New Scene");
+    }
+    public void Fifth(){
+        //SceneManager.LoadScene("New Scene");
+    }
+    public void Sixth(){
+        //SceneManager.LoadScene("New Scene");
+    }
+    public void Seventh(){
+        //SceneManager.LoadScene("New Scene");
+    }
+    public void Eighth(){
+        //SceneManager.LoadScene("New Scene");
+    }
+
+
+
 }
