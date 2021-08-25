88c81f56ef111d44c1f451102e8fa3e39b0631f1
Ryuhyis <76957700+Ryuhyis@users.noreply.github.com>
2021-08-25 15:15:32 +0900

    on/off

--- Assets/utils/Scripts/ShowCaption.cs
+++ Assets/utils/Scripts/ShowCaption.cs
@@ -5,10 +5,14 @@ using UnityEngine.UI;
 
 public class ShowCaption : MonoBehaviour
 {
+    private GameObject Off;
+    private GameObject On;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Off = GameObject.Find("Off");
+        On = GameObject.Find("On");
+        Off.SetActive(false);
     }
 
     // Update is called once per frame
@@ -17,8 +21,18 @@ public class ShowCaption : MonoBehaviour
         
     }
 
-    public void captionShow()
+    public void captionClose()
     {
         this.gameObject.SetActive(false);
+        On.SetActive(false);
+        Off.SetActive(true);
+
+    }
+
+    public void captionShow()
+    {
+        this.gameObject.SetActive(true);
+        On.SetActive(true);
+        Off.SetActive(false);
     }
 }
