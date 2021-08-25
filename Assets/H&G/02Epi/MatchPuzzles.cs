942b67e557be1bc9c4369f64decc157e56a9391a
ooo2o5 <88296556+ooo2o5@users.noreply.github.com>
2021-08-25 14:41:06 +0900

    Epi2

--- Assets/H&G/02Epi/MatchPuzzles.cs
+++ Assets/H&G/02Epi/MatchPuzzles.cs
@@ -41,10 +41,10 @@ public class MatchPuzzles : MonoBehaviour{
         if (Input.GetMouseButtonUp(0))
         {                                                                             //화면에서 마우스 클릭을 떼면
             if (col.tag == this.tag)
-            {                                                                               //충돌체의 태그와 현재 오브젝트(퍼즐조각)의 태그가 같다면
+            {                                                              //충돌체의 태그와 현재 오브젝트(퍼즐조각)의 태그가 같다면
                 col.gameObject.SetActive(false);                                                                    //충돌체 비활성화
-                Destroy(this.gameObject);                                                                           //현재 오브젝트 없애기  
-                mgo_GameControl.GetComponent<CheckAnswerEpi2>().v_CountAnswer();                                    //정답 개수를 올려주기 위한 함수 실행
+                Destroy(this.gameObject);                                                                          //현재 오브젝트 없애기  
+                mgo_GameControl.GetComponent<CheckAnswerEpi2>().v_CountAnswer();                                     //정답 개수를 올려주기 위한 함수 실행
             }
         }
     }
---
+++ Assets/H&G/02Epi/Sounds/SoundofS2.cs
@@ -0,0 +1,46 @@
+/*
+ * - Name : SoundodS2.cs
+ * - Writer : 이병권
+ * - Content : 헨젤과그레텔Epi_2 - 1. 마우스로 문을 클릭하여 걸어 갈때
+ *                               - 2. 마우스로 시작 버튼을 눌렀을 때 미니게임 시작 할때
+ * - HISTORY (수정기록)
+ * 2021-08-25 : 코드 작성 
+ * 2021-08-26 : 주석 작성
+ * 
+ * <Function>
+ * AudioSource : 소리를 불러오는 하나의 장치
+ * PlaySound : 소리를 저장하면 그 소시를 불러오는 장치
+ * audioAttack : 공격을 할때 소리를 불러오는 장치
+ * audioDie : 케릭터가 죽었을 때 소리를 불러오는 장치
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundofS2 : MonoBehaviour
+{
+
+  public AudioClip audioWalk;                // 문을 선택하여 걸어 갈때
+
+   public AudioClip audioEnterGame1;              // 영어를 선택하였을 때
+   AudioSource audioSource;                     // 두 경우를 하나의 저장해서 다른 곳에서 불러오게 한다 
+
+   void Awake(){
+
+       this.audioSource = GetComponent<AudioSource>();
+   }
+ 
+   public void PlaySound(string action) {
+       switch(action) {
+            case "Walk":                                   // 한국어를 선택하였을 때
+                audioSource.clip = audioWalk;
+                break;
+            case "Game1":                                // 영어를 선택하였을 때
+                audioSource.clip = audioEnterGame1;
+                break;
+        }
+        audioSource.Play();
+   }   
+
+}
