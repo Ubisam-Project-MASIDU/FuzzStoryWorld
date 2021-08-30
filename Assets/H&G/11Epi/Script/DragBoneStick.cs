42b692a55389de2fde401e646f2a309492358d75
ooo2o5 <88296556+ooo2o5@users.noreply.github.com>
2021-08-30 14:00:27 +0900

    123123123123124124124124

--- Assets/H&G/11Epi/Script/DragBoneStick.cs
+++ Assets/H&G/11Epi/Script/DragBoneStick.cs
@@ -2,15 +2,17 @@
  * - Name : DragBoneStick.cs
  * - Writer : 이병권
  * - Content : 헨젤과그레텔Epi11 - 마우스로 뼈다귀를 이용 할 수 있는 스크랩
-                                   (핵심 내용)
+ *                                  (핵심 내용)
  *                                 -1) 마우스로 오브젝트를 움직이게 한다
  *                                 -2) 마우스에서 손을 떼었을 때 오브젝트가 원래위치로 이동
- 
+ *                                 -3) 마우스에 오브젝트를 눌렀을 때 소리가 난다.
+ *
  * - HISTORY (수정기록)
  * 1) 2021-08-5 : 초기 개발
  * 2) 2021-08-9 : 파일 인코딩 수정
  * 3) 2021-08-11 : 주석 처리 수정
- 
+ * 4) 2021-08-30 : 소리 수정
+ *
  * - <Variable> 쓴것들에 대하여 설명
  * 1) mv2_mouseDragPosition        마우스 위치를 저장하는 벡터
  * 2) mv2_worldObjectPosition      카메라의 월드좌표로 변환을 위한 벡터
@@ -37,12 +39,12 @@ public class DragBoneStick : MonoBehaviour
     GameObject mg_SoundManager;
 
     void Start(){
-        mg_SoundManager = GameObject.Find("SoundManager");                 // 사운드 매니저 게임오브젝트 연결
+        mg_SoundManager = GameObject.Find("SoundManager");                                  // 사운드 매니저 게임오브젝트 연결
     }
 
     // 오브젝트를 클릭한 경우
     private void OnMouseDown(){
-        mg_SoundManager.GetComponent<SoundManager>().playSound("Clik3");   // 시작 버튼 효과음 재생
+        mg_SoundManager.GetComponent<SoundManager>().playSound("Clik3");                    // 들었을 때 소리 나게 하기
         screenSpace = Camera.main.WorldToScreenPoint(transform.position);
         offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenSpace.z));
     }
