9d0d837385c3db40c3783e02598222cddcd4eb32
Ryuhyis <76957700+Ryuhyis@users.noreply.github.com>
2021-08-30 11:02:51 +0900

    주석

--- Assets/H&G/09Epi/ObjectClick.cs
+++ Assets/H&G/09Epi/ObjectClick.cs
@@ -105,9 +105,9 @@ public class ObjectClick : MonoBehaviour {
         mg_Witch = GameObject.Find("witch");
         // mn_count 초기화
         mn_count = 0;
+        // 사운드 매니저 게임오브젝트 연결
+        mg_SoundManager = GameObject.Find("SoundManager");
         // 헨젤과 그레텔이 처음에 초기 위치에 있게 하기 위함.
-        mg_SoundManager = GameObject.Find("SoundManager");                 // 사운드 매니저 게임오브젝트 연결
-    
         v_SetPos();
     }
 
--- Assets/H&G/09Epi/SceneControl.cs
+++ Assets/H&G/09Epi/SceneControl.cs
@@ -26,10 +26,8 @@
  * mspr_Witch                                             마녀 스프라이트 연결을 위한 변수
  * 
  * - Function
- * v_GotoDoor()                                     문을 클릭해서 문에 헨젤과 그레텔이 다다를수있게 하는 함수
- * v_TutorialText()                                 문 클릭 이벤트 지시를 도와주기 위한 튜토리얼 텍스트와 애니메이션을 활성화해주는 함수
- * v_ChangeNextScene()                              다음 씬으로 넘어가기 위한 함수
- * v_ChangeNextSceneWhenSkipGame()                  게임이 스킵되는경우 다음씬으로 넘어가기 위한 함수
+ * v_ColorChange                                          클릭 오브젝트들을 제외하고 배경을 어둡게 해주는 함수
+ * 
  */
 using System.Collections;
 using System.Collections.Generic;
@@ -86,10 +84,10 @@ public class SceneControl : MonoBehaviour{
             // 스토리 진행하는 한국어 자막 변경
             ms_Temp2 = mvm_VoiceManager.mlva_LanguageVoices[0].mvifl_setVoiceInfoList[14].words;
             mt_Text.text = "\n        " + ms_Temp2 + "         \n";
-            //mt_Text.text = "\n       사실 그 할머니는 아이들을 잡아먹는 무서운 마녀였어요.        \n"; 
         }
         // 세번째 나레이션의 실행 조건 검사
         if (mvm_VoiceManager.isPlaying() == false && mb_PlaySecondVoice && !mb_PlayThirdVoice){
+            // 스킵게임이 아닐때만 음성 및 자막 출력
             if (PlayerPrefs.GetInt("SkipGame") == 0){
                 mvm_VoiceManager.playVoice(15);
                 mcc_CaptionControl.mn_VoiceIndex = 15;
@@ -97,16 +95,16 @@ public class SceneControl : MonoBehaviour{
                 mcc_CaptionControl.mt_CaptionText.GetComponent<Text>().text = ms_CaptionTemp;
                 ms_Temp2 = mvm_VoiceManager.mlva_LanguageVoices[0].mvifl_setVoiceInfoList[15].words;
                 mt_Text.text = "\n        " + ms_Temp2 + "         \n";
-                //mt_Text.text = "\n       물체를 클릭해서 마녀에게로부터 헨젤과 그레텔을 숨겨주세요.        \n";
                 mb_PlayThirdVoice = true;
             }
             if(mb_ChangeColorOnce == false){
                 mb_ChangeColorOnce = true;
-                Invoke("ColorChange", 2f);
+                Invoke("v_ColorChange", 2f);
             }
         }
     }
-    void ColorChange(){
+    //클릭 오브젝트들을 제외하고 배경을 어둡게 해주는 함수
+    void v_ColorChange(){
         mspr_BackGrouond.color = new Color(75 / 255f, 75 / 255f, 75 / 255f, 255 / 255f);
         mspr_Witch.color = new Color(85 / 255f, 85 / 255f, 85 / 255f, 255 / 255f);
         mb_ObjClickStart = true;
